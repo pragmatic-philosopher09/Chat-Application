752e823d0676be90c085c58825daf7dca4f94b4d
Priyanshu Mohanty <prince91001@gmail.com>
2022-08-12 22:05:56 +0530

    Server constructed ✅ Client ongoing ❗❗

--- Client.java
+++ Client.java
@@ -1,4 +1,32 @@
+import java.net.*;
+
+import java.io.*;
+
 public class Client {
+
+    Socket socket;
+    
+    BufferedReader br;
+    PrintWriter out;
+    public Client()
+    {
+        try{
+
+            System.out.println("SENDING REQUEST TO SERVER ...");
+
+            socket = new Socket("127.0.0.1",7777);
+
+            System.out.println("CONNECTION DONE!");
+
+
+
+        }
+
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         System.out.println("This Is Client...");
     }
--- Server.java
+++ Server.java
@@ -21,12 +21,79 @@ class Server
         br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
         out = new PrintWriter(socket.getOutputStream());
+
+        startReading();
+        startWriting();
     }
 
         catch(Exception e)
         {
             e.printStackTrace();
         }
+    }
+
+    public void startReading()
+    {
+       // thread - will read and return
+
+       Runnable r1 = () -> {
+          
+         System.out.println("READER STARTED...");
+
+         while(true)
+         {
+          
+            try {
+               String message = br.readLine();
+
+                if(message.equals("Quit!"))
+            {
+                System.out.println("CLIENT HAS TERMINATED CHAT!!");
+                break;
+            }
+            System.out.println("CLIENT :"+message);
+            } 
+            
+            catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            
+         }
+       };
+
+       new Thread(r1).start();
+    }
+
+    public void startWriting()
+    {
+         //thread - takes from user and sends to client
+        Runnable r2 = () -> {
+          
+            while(true)
+            {
+
+                try{
+
+                    System.out.println("WRITER STARTED...");
+                    
+                   BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+
+                   String content = br1.readLine();
+
+                   out.println(content);
+                   out.flush();
+
+                }
+
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        new Thread(r2).start();
     }
     public static void main(String args[])
     {
